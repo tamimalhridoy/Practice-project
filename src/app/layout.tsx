ce5e12794bac3d6626fdccd7bcd11301af2b3243
Tamim Al Hridoy <162357537+tamimalhridoy@users.noreply.github.com>
2025-09-18 22:03:23 +0600

    check css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
